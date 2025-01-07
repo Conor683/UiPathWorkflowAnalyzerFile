9b7b01928b2c9521b8f692fcda19d4ffc003bdd7
Conor683 <conormontgomery0204@gmail.com>
2025-01-07 16:57:07 +0000

    v1.0.5
    -new version

--- obj/Debug/net8.0/WorkflowAnalyzerTST.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorkflowAnalyzerTST.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkflowAnalyzerTST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5567fd4d646b980cb37aac71a7bfc89d16bb4d58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e604eb3cc6f3e5cd1421859e7819c03dbd8977a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkflowAnalyzerTST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkflowAnalyzerTST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
