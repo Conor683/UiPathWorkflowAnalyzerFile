7e3c94eb2ee80f05b6a10d8c4f5bc384f09fde82
Conor683 <conormontgomery0204@gmail.com>
2025-01-26 23:57:51 +0000

    it's late in the night rn
    - first attempt at outputting logs
    - New build

---
+++ Logs.cs
@@ -0,0 +1,52 @@
+﻿using UiPath.Studio.Activities.Api.Analyzer.Rules;
+using UiPath.Studio.Analyzer.Models;
+
+namespace WorkflowAnalyzerRules
+{
+    public class Logs
+    {
+
+        // This static class is not mandatory. It just helps organizining the code.
+        internal static class OutputLogMessages
+        {
+            // This should be as unique as possible, and should follow the naming convention.
+            private const string RuleId = "SG-LOG-001";
+            internal static Rule<IActivityModel> Get()
+            {
+                var rule = new Rule<IActivityModel>("Log Message", RuleId, Inspect)
+                {
+                    RecommendationMessage = "The contents of a log message.",
+                    /// Off and Verbose are not supported.
+                    ErrorLevel = System.Diagnostics.TraceLevel.Info,
+                };
+                return rule;
+            }
+
+            // This is the function that executes for each activity in all the files. Might impact performance.
+            // The rule instance is the rule provided above which also contains the user-configured data.
+            private static InspectionResult Inspect(IActivityModel activity, Rule ruleInstance)
+            {
+                var messageList = new List<string>();
+                if ((activity.ToolboxName.ToLower().Contains("logmessage")))
+                {
+                    messageList.Add($"{activity.DisplayName} - {activity.Arguments.First()}");
+                }
+                if (messageList.Count > 0)
+                {
+                    return new InspectionResult()
+                    {
+                        ErrorLevel = ruleInstance.ErrorLevel,
+                        HasErrors = true,
+                        RecommendationMessage = ruleInstance.RecommendationMessage,
+                        // When inspecting a model, a rule can generate more than one message.
+                        Messages = messageList
+                    };
+                }
+                else
+                {
+                    return new InspectionResult() { HasErrors = false };
+                }
+            }
+        }
+    }
+}
--- obj/Debug/net8.0/WorkflowAnalyzerRulesFile.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorkflowAnalyzerRulesFile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkflowAnalyzerRulesFile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b94f2e606fe6bf8da8742c4b4116cc5a14bfee5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4883f26b45196ab07c268e5663d602cc1a5883")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkflowAnalyzerRulesFile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkflowAnalyzerRulesFile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
